7ddd0856b9878f237237319c7c6f956f79f2ec25
Mark <hendriksen-mark@hotmail.com>
2024-09-28 12:00:21 +0200

    Create NTTB_scorenboord.ino

---
+++ NTTB_scorenboord/NTTB_scorenboord.ino
@@ -0,0 +1,52 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
+ESP8266WebServer server(80);
+
+#define PROJECT_NAME "NTTB"
+
+
+void setup() {
+  Serial.begin(19200);
+  WiFi.mode(WIFI_STA);
+  WiFiManager wifiManager;
+  wifiManager.setConfigPortalTimeout(120);
+  wifiManager.autoConnect(PROJECT_NAME);
+  wifiManager.setConnectTimeout(30);
+  WiFi.hostname(PROJECT_NAME);
+
+  server.on("/sbsetting", sbsetting);
+  server.on("/sblogo", sblogo);
+  server.on("/sbdata", sbdata);
+
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
+
+void sbsetting() {
+  Serial.printf("num args: %d, on url: /sbsetting \n",server.args());
+  for (int i = 0; i < server.args(); i++) {
+    Serial.printf("args: %s = %s\n", server.argName(i).c_str(), server.arg(i).c_str());
+  }
+  server.send(200, "text/plain", "test headers");
+}
+
+void sblogo() {
+  Serial.printf("num args: %d, on url: /sblogo \n",server.args());
+  for (int i = 0; i < server.args(); i++) {
+    Serial.printf("args: %s = %s\n", server.argName(i).c_str(), server.arg(i).c_str());
+  }
+  server.send(200, "text/plain", "test headers");
+}
+
+void sbdata() {
+  Serial.printf("num args: %d, on url: /sbdata \n",server.args());
+  for (int i = 0; i < server.args(); i++) {
+    Serial.printf("args: %s = %s\n", server.argName(i).c_str(), server.arg(i).c_str());
+  }
+  server.send(200, "text/plain", "test headers");
+}
